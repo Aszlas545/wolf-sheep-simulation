f0cc487b0e265afd3a6f7314cf77146785014932
Andrzej Szlaski <242545@edu.p.lodz.pl>
2023-11-30 20:59:31 +0100

    removed animate.py file as it should not be included in final solution (will be moved later to a different branch ment for learning libraries and testing), validation of all argparse arguments that required it, argument --log is validation is being done by library itself and --wait does not need validation, content of ini file given in --config argument is done all required conditions are checked and validation has been tested

--- main.py
+++ main.py
@@ -1,62 +1,108 @@
 import simulation
 from pathlib import Path
-import animate
-import utils
 import argparse
 from argparse import RawTextHelpFormatter
 import configparser
 
+# configuration of argument parser
 arg_parser = argparse.ArgumentParser(prog="Wolf and sheep simulation",
-                                     description=utils.description,
+                                     description="The program preforms a simple simulation that consists of rounds "
+                                                 "where in the first phase of the each round "
+                                                 "all the sheep move in one of four random directions "
+                                                 "and in the second phase the wolf makes it's move to the closest one "
+                                                 "trying to eliminate all sheep.",
                                      formatter_class=RawTextHelpFormatter,
                                      exit_on_error=False)
 
 arg_parser.add_argument('-c', '--config',
-                        help="Takes path to an ini file containing the configuration for the simulation.",
+                        metavar='FILE',
+                        help="takes path to an ini file containing the configuration for the simulation",
                         type=str)
 arg_parser.add_argument('-l', '--log',
-                        help="Takes int value standing for that level of logging.",
+                        help="takes int value standing for that level of logging",
+                        metavar='LEVEL',
                         type=int,
-                        choices=[10, 20, 30, 40, 50],
+                        choices=[10, 20],
                         default=0)
 arg_parser.add_argument('-r', '--rounds',
-                        help="Takes int value standing for amount rounds that the simulation will perform before ending.",
+                        metavar='NUM',
+                        help="takes int value standing for amount rounds that the simulation "
+                             "will perform before ending",
                         type=int,
                         default=50)
 arg_parser.add_argument('-s', '--sheep',
-                        help="Takes int value standing for amount of sheep that will be generated in the simulation.",
+                        metavar='NUM',
+                        help="takes int value standing for amount of sheep that will be generated in the simulation",
                         type=int,
                         default=15)
 arg_parser.add_argument('-w', '--wait',
-                        help="If supplied after each round of simulation the program will wait\n"
-                             "for any keyboard input from the user before the next round is preformed.",
+                        help="after each round program will wait for ant user input",
                         action='store_true')
 
+# parsing arguments given in the command line execution
 args = arg_parser.parse_args()
 
-config_file = args.config
-log_level = args.log
-rounds_amt = args.rounds
-sheep_amt = args.sheep
-do_wait = args.wait
+wolf_move_dst = 1.0
+sheep_move_dst = 0.5
+spawn_border = 10
 
-if not Path.exists(config_file):
-    arg_parser.error("The file %s does not exist!" % config_file)
-if not Path.is_file(config_file):
-    arg_parser.error("Given string %s does not lead to a file!" % config_file)
-if not config_file.endswith('.ini'):
-    arg_parser.error("The file %s is not an ini file!" % config_file)
+# checking --config argument and validating arguments passed in given file
+if args.config is not None:
+    # checking if the give path exists, leads to a file and if it does it is in ini format
+    if not Path.exists(Path(args.config)):
+        arg_parser.error("The file %s does not exist!" % args.config)
+    if not Path.is_file(Path(args.config)):
+        arg_parser.error("Given string %s does not lead to a file!" % args.config)
+    if not args.config.endswith('.ini'):
+        arg_parser.error("The file %s is not an ini file!" % args.config)
 
-# conf_parser = configparser.ConfigParser()
-# conf_parser.read(config_file)
-#
-# wolf_move_dst = conf_parser['Wolf'].getfloat('MoveDist')
-# sheep_move_dst = conf_parser['Sheep'].getfloat('MoveDist')
-# spawn_border = conf_parser['Sheep'].getfloat('InitPosLimit')
-# print(config_file, log_level, rounds_amt, sheep_amt, do_wait)
-# print(wolf_move_dst, sheep_move_dst, spawn_border)
+    # parsing configuration of file given in --config option
+    conf_parser = configparser.ConfigParser()
+    conf_parser.read(args.config)
+
+    # checking if wolf moving distance exists in given file, and if it is a positive float
+    if conf_parser.has_option('Wolf', 'MoveDist'):
+        try:
+            wolf_move_dst = conf_parser['Wolf'].getfloat('MoveDist')
+        except:
+            raise TypeError("value at [Wolf].MoveDist was not a float")
+        if not wolf_move_dst > 0:
+            raise ValueError("float at [Wolf].MoveDist must be positive")
+    else:
+        raise configparser.NoOptionError('MoveDist', 'Wolf')
+
+    # checking if sheep initial spawn position limit exists in given file, and if it is a positive float
+
+    if conf_parser.has_option('Sheep', 'InitPosLimit'):
+        try:
+            spawn_border = conf_parser['Sheep'].getfloat('InitPosLimit')
+        except:
+            raise TypeError(f"value at [Sheep].InitPosLimit was not a float")
+        if not spawn_border > 0:
+            raise ValueError("float at [Sheep].InitPosLimit must be positive")
+    else:
+        raise configparser.NoOptionError('InitPosLimit', 'Sheep')
+
+    # checking if sheep moving distance exists in given file, and if it is a positive float
+    if conf_parser.has_option('Sheep', 'MoveDist'):
+        try:
+            sheep_move_dst = conf_parser['Sheep'].getfloat('MoveDist')
+        except:
+            raise TypeError("value at [Sheep].MoveDist was not a float")
+        if not sheep_move_dst > 0:
+            raise ValueError("float at [Sheep].MoveDist must be positive")
+    else:
+        raise configparser.NoOptionError('MoveDist', 'Sheep')
+
+# --rounds argument check
+if args.rounds < 1:
+    arg_parser.error("Amount of rounds must be a positive integer")
+# --sheep argument check
+if args.sheep < 1:
+    arg_parser.error("Amount of sheep must be a positive integer")
+
+print(wolf_move_dst, sheep_move_dst, spawn_border)
 
 simulation = simulation.Simulation()
-animate.animate(simulation, 50)
-# for i in range(100):
-#     simulation.simulate_round()
+for i in range(50):
+    simulation.simulate_round()
--- simulation.py
+++ simulation.py
@@ -9,76 +9,78 @@ class Simulation:
 
     def __init__(self):
         self.noRound = 0
-        self.sheepAlive = []
+        self.sheep_alive = []
         self.wolf = wolf.Wolf()
+        self.json_data = None
         for i in range(50):
-            self.sheepAlive.append(sheep.Sheep())
-        self.sheepStatus = self.sheepAlive.copy()
+            self.sheep_alive.append(sheep.Sheep())
+        self.sheep_status = self.sheep_alive.copy()
         with open("logs/alive.csv", "w", newline=""):
             pass
         with open("logs/pos.json", "w", newline="") as jsonfile:
             json.dump([], jsonfile, indent=4)
+        with open("logs/pos.json", "r", newline="") as jsonfile:
+            self.json_data = json.load(jsonfile)
         with open("logs/log.log", "w", newline=""):
             pass
 
 
     def simulate_round(self):
-        for animal in self.sheepAlive:
+        for animal in self.sheep_alive:
             animal.move()
-        val = self.wolf.move(self.sheepAlive)
+        val = self.wolf.move(self.sheep_alive)
         if self.wolf.position == val.position:
-            self.sheepAlive.remove(val)
+            self.sheep_alive.remove(val)
             val.position = [None, None]
         self.noRound += 1
         print("round:", self.noRound,
               "wolf x:", round(self.wolf.position[0], 3),
               "wolf y:", round(self.wolf.position[1], 3),
-              "sheep alive:", len(self.sheepAlive),
+              "sheep alive:", len(self.sheep_alive),
               "eaten: " if val.position == [None, None]
-              else "chasing:", self.sheepStatus.index(val))
+              else "chasing:", self.sheep_status.index(val))
         self.log_to_csv()
         self.log_to_json()
         self.log_to_txt()
-        if len(self.sheepAlive) == 0:
+        if len(self.sheep_alive) == 0:
             quit("All sheep were slaughtered! " + str(self.noRound) + " rounds were performed")
 
     def get_sheep_x(self):
         collection = []
-        for s in self.sheepAlive:
+        for s in self.sheep_alive:
             collection.append(s.position[0])
         return collection
 
     def get_sheep_y(self):
         collection = []
-        for s in self.sheepAlive:
+        for s in self.sheep_alive:
             collection.append(s.position[1])
         return collection
 
     def get_sheep_coords(self):
         collection = []
-        for s in self.sheepAlive:
+        for s in self.sheep_alive:
             collection.append(s.position)
         return collection
 
     def log_to_csv(self):
         with open("logs/alive.csv", "a", newline="") as csvfile:
             writer = csv.writer(csvfile, delimiter=";")
-            writer.writerow([self.noRound, self.sheepAlive.__len__()])
+            writer.writerow([self.noRound, self.sheep_alive.__len__()])
 
     def log_to_json(self):
         with open("logs/pos.json", "r+", newline="") as jsonfile:
             sheep_data = []
-            for s in self.sheepStatus:
+            for s in self.sheep_status:
                 sheep_data.append((s.position[0], s.position[1]))
             no_round = {"no_round": self.noRound}
             wolf_pos = {"wolf_pos": (self.wolf.position[0], self.wolf.position[1])}
             sheep_pos = {"sheep_pos": sheep_data}
             data = [no_round, wolf_pos, sheep_pos]
 
-            dictionary = json.load(jsonfile)
-            dictionary.append(data)
+            self.json_data.append(data)
             jsonfile.seek(0)
-            json.dump(dictionary, jsonfile, indent=4)
+            json.dump(self.json_data, jsonfile, indent=4)
 
     def log_to_txt(self):
         with open("logs/log.log", "a", newline="") as logfile:
