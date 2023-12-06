4af546ffb187346761a5cc2de46797926196a0f0
Aszlas545 <242545@edu.p.lodz.pl>
2023-12-06 17:04:08 +0100

    changed sheep alive attribute

--- wolf_sheep_simulation/simulation.py
+++ wolf_sheep_simulation/simulation.py
@@ -7,15 +7,16 @@ class Simulation:
 
     def __init__(self, wolf_speed, sheep_spawn_limit, sheep_speed, sheep_amount, do_wait):
         self.no_round = 1
-        self.sheep_alive = []
+        self.sheep_status = []
         self.wolf = wolf.Wolf(wolf_speed)
         self.json_data = None
         self.do_wait = do_wait
+        self.sheep_alive = sheep_amount
 
         for i in range(sheep_amount):
-            self.sheep_alive.append(sheep.Sheep(sheep_spawn_limit, sheep_speed, i))
+            self.sheep_status.append(sheep.Sheep(sheep_spawn_limit, sheep_speed, i))
         logging.info("position of all sheep was established")
-        self.sheep_status = self.sheep_alive.copy()
+
 
         logger.overwrite_csv()
 
@@ -23,25 +24,26 @@ class Simulation:
 
     def simulate_round(self):
         logging.info("round %i was started" % self.no_round)
-        for animal in self.sheep_alive:
-            animal.move()
+        for animal in self.sheep_status:
+            if not animal.position == [None, None]:
+                animal.move()
         logging.info("all sheep moved")
 
         wolf_target = self.wolf.move(self.sheep_status)
 
         if self.wolf.position == wolf_target.position:
-            self.sheep_alive.remove(wolf_target)
+            self.sheep_alive -= 1
             wolf_target.position = [None, None]
             logging.info("sheep %i was eaten" % self.sheep_status.index(wolf_target))
 
         print("round:", self.no_round,
               "wolf x:", round(self.wolf.position[0], 3),
               "wolf y:", round(self.wolf.position[1], 3),
-              "sheep alive:", len(self.sheep_alive),
+              "sheep alive:", self.sheep_alive,
               "eaten: " if wolf_target.position == [None, None]
               else "chasing:", self.sheep_status.index(wolf_target))
 
-        logger.log_to_csv([self.no_round, len(self.sheep_alive)])
+        logger.log_to_csv([self.no_round, self.sheep_alive])
         logging.debug("data was logged to csv file")
 
         sheep_data = []
@@ -55,7 +57,7 @@ class Simulation:
         logging.debug("data was logged to json file")
 
         logging.info("the round %i is about to end, the number of sheep alive: %i"
-                          % (self.no_round, len(self.sheep_alive)))
+                          % (self.no_round, self.sheep_alive))
         if self.do_wait:
             input("waiting for input")
 
@@ -63,7 +65,7 @@ class Simulation:
         for i in range(rounds_amt):
             self.simulate_round()
             self.no_round += 1
-            if len(self.sheep_alive) == 0:
+            if self.sheep_alive == 0:
                 print("All sheep were eaten")
                 logging.info("the simulation terminates due to all sheep being eaten")
                 return
